e5aac53890680e31568d088a6307269118e86c01
1polygon <bennija@web.de>
2025-01-29 21:36:34 +0100

    build

--- README.md
+++
@@ -1,4 +0,0 @@
-## CatmullRom-Spline
-A small demo of a catmull-rom spline with constant speed follower.
-
-![Screen Shot 2025-01-29 at 21 07 30](https://github.com/user-attachments/assets/7ba61a5b-9d82-4ffd-aef6-25fa82b84fbb)
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { SplineController } from './controller';
-import { CatmullRomSpline } from './catmullrom';
-import './style.css'
-
-const canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-
-const spline = new CatmullRomSpline(canvas);
-const controller = new SplineController(spline);
